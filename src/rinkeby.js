19997b717b7e3b767ac6a19d878673ebe6a2472b
pythonpete32 <pythonpete32@gmail.com>
2020-07-17 22:06:55 +0100

    add rinkeby script

--- src/rinkeby.js
+++ src/rinkeby.js
@@ -3,27 +3,25 @@ const {encodeActCall, execAppMethod} = require('mathew-aragon-toolkit');
 const ethers = require('ethers');
 const utils = require('ethers/utils');
 const {keccak256} = require('web3-utils');
-
 const {RLP} = utils;
 const provider = ethers.getDefaultProvider('rinkeby');
 const BN = utils.bigNumberify;
 const env = 'rinkeby';
 
+
 // DAO addresses
-const dao = '0x2C4f3fa8Da1843EEaa1e56Eba505b5aA335fb5EA';
-const acl = '0xfecab8b3885d7b17bc2f7c09b4b5080ba5fd6357';
-const agent = '0x970708b722b75c9ffa6a3817f51b27eb033b9d1c';
-const tokenManager = '0xc68be21fc6f9cd41ea97e74e8845edea520f39c4';
-const voting = '0x395e1eb7285e786a18d63ada9aeef9dfacb3e2cd';
-const daoShopToken = '0x20431b540f52237e857e6b0a9e5d07b33c3dea8c';
+const dao = 'YOUR_DAO_ADDRESS';
+const acl = 'YOUR_DAO_ACL_ADDRESS';
+const agent = 'YOUR_DAO_AGENT_ADDRESS';
+const tokenManager = 'YOU_DAO_TOKEN_MANAGER_ADDRESS';
+const voting = 'YOUR_DAO_VOTING_ADDRESS';
 const ANY_ADDRESS = '0xffffffffffffffffffffffffffffffffffffffff';
+const ETH_ADDRESS = '0x0000000000000000000000000000000000000000';
 
 
-
-// new apps
+// new apps ***Note these addresses are different on Rinkeby and Mainnet***
 const redemptionsAppId = '0x743bd419d5c9061290b181b19e114f36e9cc9ddb42b4e54fc811edb22eb85e9d';
 const redemptionsBase = '0xe47d2A5D3319E30D1078DB181966707d8a58dE98';
-
 let redemptions;
 
 
@@ -32,6 +30,7 @@ const newAppInstanceSignature = 'newAppInstance(bytes32,address,bytes,bool)';
 const createPermissionSignature = 'createPermission(address,address,bytes32,address)';
 const grantPermissionSignature = 'grantPermission(address,address,bytes32)';
 const redemptionsInitSignature = 'initialize(address,address,address[])';
+
 
 // functions for counterfactual addresses
 async function buildNonceForAddress(_address, _index, _provider) {
@@ -49,16 +48,15 @@ async function calculateNewProxyAddress(_daoAddress, _nonce) {
 
 async function firstTx() {
     // counterfactual addresses
-    const nonce1 = await buildNonceForAddress(dao, 0, provider);
-    const newAddress1 = await calculateNewProxyAddress(dao, nonce1);
-    redemptions = newAddress1;
-
+    const nonce = await buildNonceForAddress(dao, 0, provider);
+    redemptions = await calculateNewProxyAddress(dao, nonce);
+    
 
     // app initialisation payloads
     const redemptionsInitPayload = await encodeActCall(redemptionsInitSignature, [
         agent,
         tokenManager,
-        [daoShopToken]
+        [ETH_ADDRESS]
     ]);
 
     // package first transaction
@@ -130,7 +128,7 @@ async function firstTx() {
         [
             script,
             `
-            this is something
+            Installing Redemptions
             `,
         ],
         () => {},
